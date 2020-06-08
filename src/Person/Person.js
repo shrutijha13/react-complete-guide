db0f6a2d3e9e855f88fca182fc3b10f97cd46ab4
Shruti Jha <shruti.jha13@gmail.com>
2020-06-09 02:25:24 +0530

    @Learning about CSS Modules

---
+++ src/Person/Person.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './Person.css';
+
+// const StyledDiv = styled.div`
+// 	width: 60%;
+// 	border: 1px solid #eee;
+// 	margin: 16px auto;
+// 	box-shadow: 0 2px 3px #ccc;
+// 	padding: 16px;
+// 	text-align: center;
+// 	@media (min-width: 500px) {
+// 		width: 450px;
+// }`;
+
+const person = (props) => {
+	// const style = {
+	// 	'@media (min-width: 500px)': {
+	// 		width: '450px'
+	// 	}
+	// };
+	return (
+		<div className={classes.Person}>
+			<p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+			<p>{props.children}</p>
+			<input type="text" onChange={props.changed} value={props.name}/>
+		</div>
+	)
+};
+
+export default person;
