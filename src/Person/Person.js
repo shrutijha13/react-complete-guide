1d6a5db3fba7ebb0d9214c9965660e02883c9773
Shruti Jha <shruti.jha13@gmail.com>
2020-06-10 21:10:48 +0530

    @SomethingWentWrong

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import classes from './Person.css';
 
-// const StyledDiv = styled.div`
-// 	width: 60%;
-// 	border: 1px solid #eee;
-// 	margin: 16px auto;
-// 	box-shadow: 0 2px 3px #ccc;
-// 	padding: 16px;
-// 	text-align: center;
-// 	@media (min-width: 500px) {
-// 		width: 450px;
-// }`;
-
 const person = (props) => {
-	// const style = {
-	// 	'@media (min-width: 500px)': {
-	// 		width: '450px'
-	// 	}
-	// };
 	const rnd = Math.random();
 	if( rnd > 0.7 ) {
 		throw new Error('Something went wrong');
